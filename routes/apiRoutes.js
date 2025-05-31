17123e70396dee1454dde46383e1474a30a1097c
pranavkhaspa <khaspa.pranav1@gmail.com>
2025-06-01 00:50:52 +0530

    Add GET /api/:id route to fetch a single startup by ID

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import { getAllData } from '../controllers/getAllData.js'
-import { getDataByPathParams } from '../controllers/getDataByPathParams.js'
+import express from "express";
+import { getAllData } from "../controllers/getAllData.js";
+import { getDataByPathParams } from "../controllers/getDataByPathParams.js";
+import { getSingleData } from "../controllers/getSingleData.js";
 
-export const apiRouter = express.Router()
+export const apiRouter = express.Router();
 
-apiRouter.get('/', getAllData)
-
-apiRouter.get('/:field/:term', getDataByPathParams)
+apiRouter.get("/", getAllData);
+apiRouter.get("/:id", getSingleData);
+apiRouter.get("/:field/:term", getDataByPathParams);
